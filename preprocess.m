408dc895c97d0db0369ed3cb48fddd814e3cde15
Xiaozhu Lin <linxzh@shanghaitech.edu.cn>
2023-04-16 14:13:24 +0800

    nomal commit

--- preprocess.m
+++ preprocess.m
@@ -1,5 +1,7 @@
-mat_path = 'mat_data';
+mat_path = 'data_mat/random/2';
 files_name = {dir(fullfile(mat_path,'*.mat')).name}';
+
+csv_path = 'data_csv/random/2';
 
 for index = 1:length(files_name)
     name = files_name{index,1};
@@ -7,5 +9,7 @@ for index = 1:length(files_name)
 
     disp(fullfile(mat_path, name))
     disp(strcat('rflysim_', name_split{1}, '_', 'baseline', '_', name_split{2}, '.csv'))
-    mat2csv(fullfile(mat_path, name), strcat('rflysim_', name_split{1}, '_', 'baseline', '_', name_split{2}, '.csv'))
+    
+    csv_name = strcat('rflysim_', name_split{1}, '_', 'baseline', '_', name_split{2}, '.csv');
+    mat2csv(fullfile(mat_path, name), fullfile(csv_path, csv_name))
 end
