5cfb063482fcda3120c31a120f525291131e3abd
Xiaozhu Lin <linxzh@shanghaitech.edu.cn>
2023-04-20 20:55:56 +0800

    add new dataset from rflysim

--- preprocess.m
+++ preprocess.m
@@ -1,7 +1,7 @@
-mat_path = 'data_mat/random/2';
+mat_path = 'data_mat/random/3';
 files_name = {dir(fullfile(mat_path,'*.mat')).name}';
 
-csv_path = 'data_csv/random/2';
+csv_path = 'data_csv/random/3';
 
 for index = 1:length(files_name)
     name = files_name{index,1};
